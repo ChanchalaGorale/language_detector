8b7a1ddf977fc16721b6a21be32f13d217dafc62
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2024-06-02 11:18:55 +0530

    done

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from model.model import predict_pipeline
 from model.model import __version__ as model_version
 from flask_cors import CORS, cross_origin
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,24 +12,16 @@ cors = CORS(app)
 
 @app.route("/",  methods=["GET", "POST"])
 def home():
+
     result=""
-    text = request.form['text']
+    if request.form:
+        text = request.form['text']
+        if text:
+            result = predict_pipeline(text)
 
-    result = predict_pipeline(text)
-
-   # print("text",text)
     return  render_template("home.html",result=result )
 
-@app.route("/predict" )
-def predict():
-    text = request.form['text']
 
-    print("text",text)
-    #print("text1",text)
-   # language = predict_pipeline(text)
-    #print(language)
-    #return {"language": language}
-    return "hey"
 
 if __name__ =="__main__":
     app.run(debug=True)
